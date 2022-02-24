75b9b10b18c1bedad802beac298df8093ffafb50
spicyboi2337 <abuck237@gmail.com>
2022-02-23 22:31:59 -0600

    Now using yfinance

--- Stocks.py
+++ Stocks.py
@@ -1,27 +1,15 @@
-from bs4 import BeautifulSoup
-from selenium import webdriver
-from selenium.webdriver.chrome.service import Service
+import sys        #for exit
+from time import sleep
+import time
+import yfinance as yf
 
 stock = input("Enter the ticker you would like to see data for: ")
 
-#https://stackoverflow.com/questions/44030983/yahoo-finance-url-not-working#47505102
-url = 'https://query2.finance.yahoo.com/v10/finance/quoteSummary/' + stock + '?modules=price'
+stock_info = yf.Ticker(stock).info
+market_price = stock_info['regularMarketPrice']
 
-#url = 'https://finance.yahoo.com/quote/' + stock
-s = Service(r"C:\Users\mraar\AppData\Local\EdgeDriver\edgedriver_win64\msedgedriver.exe")
+print('market price ', market_price)
 
-#Using Edge because Windows :)
-#driver = webdriver.Chrome(PATH)
-#driver.get(url)
+previous_close_price = stock_info['regularMarketPreviousClose']
 
-driver = webdriver.Edge(service = s)
-driver.get(url)
-
-#code based on https://www.thepythoncode.com/article/extract-weather-data-python
-def check_price():
-	price = BeautifulSoup(driver.page_source, 'html.parser')#parse the html code
-	temp = price.find('div', {'class':'condition-data'})#finds condition-data class to find price
-	current = temp.find('span',{'class':'wu-value wu-value-to'}).text #gets current price
-print(check_price.current)
-
-
+print('previous close price ', previous_close_price)
